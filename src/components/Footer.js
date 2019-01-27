e23258600653c57d29bcdb5be38aea977dc287ef
Jana Beck <jana.beck@stitchfix.com>
2019-01-27 12:25:51 -0600

    revised Footer now that deploying on blog subdomain

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,20 @@ import React from 'react'
 
 import { rhythm, scale } from '../utils/typography'
 
-const Footer = () => (
-  <footer
-    style={{
-      bottom: rhythm(0.125),
-      position: 'fixed',
-      right: rhythm(0.25),
-      ...scale(-0.5),
-    }}
-  >
-    {`© jana e. beck 2012–${new Date().getFullYear()}`}
-  </footer>
-)
+const Footer = () => {
+  const currentYear = new Date().getFullYear()
+  return (
+    <footer
+      style={{
+        bottom: rhythm(0.125),
+        position: 'fixed',
+        right: rhythm(0.25),
+        ...scale(-0.5),
+      }}
+    >
+      {`© jana e. beck, 2019${currentYear > '2019' ? `–${currentYear}` : ''}`}
+    </footer>
+  )
+}
 
 export default Footer
