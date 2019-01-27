9111e73d14dce992b1f9e35ad0b98373c1bc8944
Jana Beck <jana.beck@stitchfix.com>
2019-01-27 16:02:18 -0600

    add Markdown transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   siteMetadata: {
-    title: `jana e. beck`,
-    description: `personal website of jana e. beck`,
-    author: `jana e. beck`,
+    title: 'jana e. beck',
+    description: 'personal website of jana e. beck',
+    author: 'jana e. beck',
   },
   plugins: [
     {
@@ -18,32 +18,38 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: 'src/utils/typography',
       },
     },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'src',
         // eslint-disable-next-line no-undef
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-transformer-remark',
       options: {
-        name: `janabeck.com`,
-        short_name: `jana e. beck`,
-        start_url: `/`,
-        background_color: `#c7fdfa`,
-        theme_color: `#831a6f`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        plugins: ['gatsby-remark-prismjs'],
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'janabeck.com',
+        short_name: 'jana e. beck',
+        start_url: '/',
+        background_color: '#c7fdfa',
+        theme_color: '#831a6f',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
