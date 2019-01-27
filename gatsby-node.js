ead12461f035f7316ad0f22413bcebf5804f2a86
Jana Beck <jana.beck@stitchfix.com>
2019-01-27 16:01:21 -0600

    create pages from Markdown

---
+++ gatsby-node.js
@@ -0,0 +1,66 @@
+/* eslint-env node */
+
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const BlogPost = path.resolve('./src/templates/BlogPost.js')
+  return graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // create blog posts pages
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: post.node.fields.slug,
+        component: BlogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const value = createFilePath({ basePath: 'pages', node, getNode })
+    createNodeField({
+      name: 'slug',
+      node,
+      value,
+    })
+  }
+}
