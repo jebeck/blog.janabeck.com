3ed8cdf794b8a0627e39e23cffcd79e75b374e01
Jana Beck <jana.beck@stitchfix.com>
2019-01-27 20:24:30 -0600

    make PostSummary dark purple text

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants'
 
+import { darkPurple } from '../utils/colors'
 import MatchesMobile from '../utils/MatchesMobile'
 import { rhythm, scale } from '../utils/typography'
 
@@ -10,6 +11,7 @@ const SummaryP = styled.p`
   ${MOBILE_MEDIA_QUERY} {
     margin: ${rhythm(0.0625)} 0 ${rhythm(0.0625)} ${rhythm(0.25)};
   }
+  color: ${darkPurple};
   margin: ${rhythm(0.25)} 0 ${rhythm(0.25)} ${rhythm(1)};
   text-align: right;
 `
